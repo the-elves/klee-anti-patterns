602a4c32ef70493dfe096f5b28d8d10952d287b4
the-elves <ajinkya@iisc.ac.in>
2026-05-04 04:26:05 +0530

    Add time ENV VAR

--- scripts/run-eval.sh
+++ scripts/run-eval.sh
@@ -1,2 +1,2 @@
 set -xe
-scripts/evaluate_passes.py run --bench-dir /home1/ajinkya/Workspace/Garuda2/benchmarks/build/coreutils-obj-llvm/bin --max-time 5min --out-dir $1 --passes $2
+scripts/evaluate_passes.py run --bench-dir /home1/ajinkya/Workspace/Garuda2/benchmarks/build/coreutils-obj-llvm/bin --max-time $RUN_EVAL_TIME --out-dir $1 --passes $2
